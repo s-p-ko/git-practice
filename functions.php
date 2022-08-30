00aad1e7b08c001f0942f7a67f6f8a789f56ed18
Serhii Kotsiuba <s-p-ko@ukr.net>
2022-08-30 10:57:39 +0300

    Resolve conflicts in functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,12 @@
 <?php
 
-function get_profile( $id )
+function get_profile($id)
 {
-    $profile = get_user_by( ‘id’, $id );
-    return  $profile;
+    $profile = get_user_by(‘id’, $id);
+    return $profile;
+}
+
+function get_user_meta($id, $key)
+{
+    return get_metadata('user', $user_id, $key, $single);
 }
